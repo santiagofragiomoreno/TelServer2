6fce68f67d0eb91a7876f115ccbc8074d8415c22
androuz <androuz@users.noreply.github.com>
2020-07-08 18:59:33 +0200

    admin site url

--- app/telrest/telrest/urls.py
+++ app/telrest/telrest/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('mjxmyz5lpbd66gpte5d4/', admin.site.urls),
     path('', include('telapi.urls')),
 ]
