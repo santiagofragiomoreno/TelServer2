e68be6adf8e9735c3d99c10c5a790904c58e8c76
androuz <androuz@users.noreply.github.com>
2020-07-14 12:08:41 +0200

    Btn test 2

--- app/telrest/telapi/urls.py
+++ app/telrest/telapi/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     # test btns
-    path('k4ijt8p0ni06l916vntlgm72ib8nvj', views.index, name='index'),
-    path('mjnrdahjg0l6zh5ha9viipd1s8n0wn', views.main_door, name='maindoor'),
-    path('mjnrdahjg0l6zh5ha9vigm72ib8nvj', views.building_door, name='buildingdoor'),
+    path('stryujk4ijt8p0ni06l916vntlgm72ib8nvj', views.index, name='index'),
+    path('mjnrdahjg0lgtgtd6zh5ha9viipd1s8n0wn', views.main_door, name='maindoor'),
+    path('mjnrdahjg0l6zh5hayytrt9vigm72ib8nvj', views.building_door, name='buildingdoor'),
 
 
     path('payload', views.Payload.as_view(), name='payload'),
