45de8123a307c25733dcd4b24dd75dec4e3a8289
Ignacio <igna_balles@hotmail.es>
2020-10-13 12:07:54 +0200

    first configuration

--- app/telrest/login/urls.py
+++ app/telrest/login/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('ownerlog/', views.owner_log, name='owner_log'),#Panel.ehlock.com
+    path('', views.owner_log, name='owner_log'),  # Panel.ehlock.com , (ownerlog/)
     path('buscar', views.owner_log_info, name='owner'),
-    path('panel', views.owner_panel, name='panel'),#Panel.ehlock.com/owner
+    path('panel', views.owner_panel, name='panel'),  # Panel.ehlock.com/owner , (panel)
     path('createaccess', views.create_access, name='createaccess'),
     path('newreservation', views.new_reservation, name='newreservation'),
 ]
