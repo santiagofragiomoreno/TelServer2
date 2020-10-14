cb3b603ccd5b7538edd10e833c4b90955f61dc52
Román Clemente Jurado <47026722+romanclemente@users.noreply.github.com>
2020-10-14 16:41:03 +0200

    l.k

--- app/telrest/login/views.py
+++ app/telrest/login/views.py
@@ -6,9 +6,9 @@ from rest_framework.exceptions import APIException
 from security.jwt_gen import JWTEncoder
 import time
 from telapi.models import Instruction, Task, Ownership, Grant, Access, SensorData, SensorType, FlatOwner, Flat
-from .models import User_App, Reservation
 from security.permissions import IsIot, IsClient, IsSuperuser, IsOwner
 import datetime
+from .models import User_App, Reservation
 from telapi.validations import validate_date, validate_datetime, validate_clientemail, validate_integer
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
@@ -34,30 +34,33 @@ def owner_log(request):
 # ---------use for login in BBDD-------------------
 
 
-def owner_log_info(request):
+def consulting(request):
     context = {
         'ms': '',
         'user': False,
         'super': False,
         'staff': False
     }
-    global owner, owner_object
-
     if request.POST["email"] and request.POST["password"]:
         username = request.POST["email"]
         password = request.POST["password"]
         # filter search in list if exists user with username and password equals at the form
         usuario = User.objects.filter(
-            username__icontains=username, password__icontains=password)
-        owner_object = User.objects.get(username__icontains=username)
+        username__icontains=username, password__icontains=password)
+
+        user = User.objects.get(username__icontains=username)
+
+        request.session['user']=user.username
+        request.session['userid']=user.id
 
         if usuario:
-            if owner_object.is_superuser:
-                template = loader.get_template('superadmin/home.html')
+            if user.is_superuser:
+                template = loader.get_template('admin/home.html')
                 return HttpResponse(template.render(context, request))
-
+        
             else:
-                template = loader.get_template('owner/home.html')
+                context['msg'] = request.session['user']
+                template = loader.get_template('owner/navbar.html')
                 return HttpResponse(template.render(context, request))
         else:
             return HttpResponseRedirect('panel.ehlock.test/')
