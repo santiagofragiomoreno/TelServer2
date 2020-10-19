2584fa8a7e320b2d1b5158db217c06fbc5a161e1
root <root@ip-172-31-44-220.eu-west-1.compute.internal>
2020-10-19 10:33:26 +0200

    server side

--- app/telrest/telrest/settings.py
+++ app/telrest/telrest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yuo)pqhs$8q0wa^r@@9n_3x&av$^a+%-*sb+^=#5@0bn4w9wd='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.1.33', 'telgate.test', 'localhost', '127.0.0.1',
                  'telfiregate.com', 'api.openityourself.com', 'ehlock.com']
