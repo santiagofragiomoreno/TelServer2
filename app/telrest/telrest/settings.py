38c31c3ef77a174f5ad47f38eda86e1d26fa8deb
Román Clemente Jurado <47026722+romanclemente@users.noreply.github.com>
2020-10-20 10:29:35 +0200

    bbdd_commit

--- app/telrest/telrest/settings.py
+++ app/telrest/telrest/settings.py
@@ -103,16 +103,16 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'telfiregate',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
+#DATABASES = {
+#   'default': {
+#      'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'telfiregate',
+#        'USER': 'root',
+#        'PASSWORD': '',
+#        'HOST': 'localhost',
+#        'PORT': '3306',
+#    }
+#}
 
 
 # Password validation
@@ -166,7 +166,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 # pass: lfycx8ot40y4cl684uv8
 # ----------------
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'telfiregate',
@@ -175,7 +175,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
         'HOST': 'telfiregatedb.cir9kc3wxllu.eu-west-1.rds.amazonaws.com',
         'PORT': '3306',
     }
-}"""
+}
 
 # LOGIN_REDIRECT_URL = 'panel'
 LOGIN_URL = 'login'
